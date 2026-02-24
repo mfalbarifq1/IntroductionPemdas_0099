501cb38670dbe04295eecea0fe652ac8b8430922
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-02-24 16:17:43 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    int umur ;
+    umur = 20;
+
+    cout << "albar" << endl;
+    cout << "78" << endl;
+    cout << umur << endl;
+
+
+
+}
