67d0678a5dec643124f57cb3a665babac3a07fd6
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-02-24 16:20:26 +0700

    second commit

---
+++ cobalagi2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    bool cek = true ; 
+    char jk;
+    int angka = 10;
+    float tinggi = 180.5;
+    double harga = 200000.00;
+    string nama = "albar";
+
+    jk = 'L';
+    cout <<"selammat datang"<< endl;
+    cout << "di TI UMY \n";
+    cout << "yogyakrta\n";
+
+    nama = "albar";
+    cout << "nama saya : " <<nama;
+}
