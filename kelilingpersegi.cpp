79348ff56da3bbf8eb3785a14f0ed96ec7531c3d
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-02-24 16:48:07 +0700

    fifth commit

--- kelilingpersegi.cpp
+++ kelilingpersegi.cpp
@@ -2,3 +2,13 @@
 #include<iostream>
 using namespace std;
 
+int main(){
+    float sisi,keliling;
+    cout << "berapa panjang sisinya :";
+    cin>> sisi;
+
+    keliling = 4*sisi;
+
+    cout << "jadi keliling nya adalah: "<< keliling;
+
+}
