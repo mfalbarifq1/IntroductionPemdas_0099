7e1d39f46eaa5e7f97df321ac9a7705ee0914a0d
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-02-24 16:26:49 +0700

    third commit

---
+++ persegi.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std; 
+
+int main()
+{
+    float p,l,luas; 
+
+    cout << "masukkan p: ";
+    cin >> p;
+    cout << "masukkan l: ";
+    cin >> l;
+    luas = p*l;
+
+    cout << "hasilnya:" << luas ;
+
+
+
+
+
+}
